1b712a5d344ac12fb11fab32dce85b83f59ea82a
이지희 <som2plestory@naver.com>
2022-11-18 21:28:02 +0900

    221118

--- src/FindPw_17219/Main.java
+++ src/FindPw_17219/Main.java
@@ -1,5 +1,39 @@
 package FindPw_17219;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int site = Integer.parseInt(st.nextToken());
+		int find = Integer.parseInt(st.nextToken());
+		
+		Map<String, String> address = new HashMap<>(site);
+		
+		for(int i=0; i<site; i++) {
+			st = new StringTokenizer(br.readLine());
+			address.put(st.nextToken(),st.nextToken());
+		}
+		
+		for(int i=0; i<find; i++) {
+			String str = br.readLine();
+			bw.write(address.get(str)+"\n");
+		}
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
 }
